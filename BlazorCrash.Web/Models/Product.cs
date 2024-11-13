09f9a275bdf57c0f49255e49dd7173c85c1e5752
Camilo Chaves <camilo.chaves@capgemini.com>
2024-11-12 23:37:41 -0300

    Added Tables and Bindings

---
+++ BlazorCrash.Web/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorCrash.Web.Models
+{
+    public class Product
+    {
+        private decimal _price;
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                // If using an event callback or property changed notification, trigger it here
+            }
+        }
+        public string Name { get; set; }
+        public bool isActive { get; set; }
+
+        public IEnumerable<ProductProps> ProductProps { get; set; }
+    }
+}
---
+++ BlazorCrash.Web/Models/ProductProps.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorCrash.Web.Models
+{
+    public class ProductProps
+    {
+        public string Key { get; set; }
+        public string Value
+        {
+            get; set;
+        }
+    }
+}
