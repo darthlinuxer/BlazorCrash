0b27aa8cff7939aa2cc56ffc6478d0500c4d9440
DarthLinuxer <chaves.camilo@gmail.com>
2024-11-13 17:50:07 -0300

    Added Cards and Tables

--- BlazorCrash.Web/Models/Product.cs
+++ BlazorCrash.Web/Models/Product.cs
@@ -2,17 +2,11 @@
 {
     public class Product
     {
-        private decimal _price;
-        public decimal Price
-        {
-            get => _price;
-            set
-            {
-                _price = value;
-                // If using an event callback or property changed notification, trigger it here
-            }
-        }
+        public int Id { get; set; }
+        public decimal Price { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; } 
+        public string Image { get; set; }
         public bool isActive { get; set; }
 
         public IEnumerable<ProductProps> ProductProps { get; set; }
